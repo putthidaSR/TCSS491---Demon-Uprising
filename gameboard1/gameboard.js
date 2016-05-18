0e5752c161896acff512412500c3445838770492
zhouzhaoxu <zhouzhaoxu@yahoo.com>
2016-05-17 17:07:20 -0700

    Update gameboard.js

--- gameboard1/gameboard.js
+++ gameboard1/gameboard.js
@@ -4,9 +4,11 @@ function GameBoard(game, map) {
     this.removeFromWorld = false;
     this.startXArray = [];
     this.startYArray = [];
+    this.humanList = [];
     this.startx = null;
     this.starty = null;
-    this.setState();
+    this.clockTick = 0;
+    this.setState(7);
 }
 
 GameBoard.prototype = new Entity();
@@ -15,6 +17,12 @@ GameBoard.prototype.constructor = GameBoard;
 GameBoard.prototype.update = function () {
     
     Entity.prototype.update.call(this);
+    this.clockTick++;
+    if(this.clockTick > Math.random() * 1000 + 100 && this.humanList.length > 0) {
+    	this.game.addEntity(this.humanList.pop());
+        this.clockTick = 0;
+    }
+    
 }
 
 GameBoard.prototype.draw = function (ctx) {
@@ -24,10 +32,11 @@ GameBoard.prototype.getStart = function () {
     var start = Math.floor(Math.random() * this.startXArray.length);
     this.startx = this.startXArray[start] * GAME_CONSTANT.BLOCK_SIZE;
     this.starty = this.startYArray[start] * GAME_CONSTANT.BLOCK_SIZE;
+    return {x: this.startx, y: this.starty, nextDir: "a"};
 
 }
 
-GameBoard.prototype.setState = function () {
+GameBoard.prototype.setState = function (level) {
     for(var row = 0; row < this.map.length; row++) {
 	    for(var col = 0; col < this.map[row].length; col++) {
 	        if(this.map[row][col] == "s") {
@@ -37,4 +46,22 @@ GameBoard.prototype.setState = function () {
 	        }
 	    }
 	}
+    for(var populated = 0; populated < level; populated++) {
+        this.humanList.push(new Magician(this.game, this, AM.getAsset("./img/magician.png"), 
+			AM.getAsset("./img/magician2.png")));
+    }
+}
+
+GameBoard.prototype.getNextStep = function (x, y, nextDir) {
+    var xpostion = x / GAME_CONSTANT.BLOCK_SIZE;
+    var ypostion = y / GAME_CONSTANT.BLOCK_SIZE; 
+    if(xpostion == 0) {
+        ypostion++;
+    } else if (ypostion == 0) {
+        xpostion++;
+    }
+    if(nextDir == "r") {
+        ypostion++;
+    }
+    return {x: xpostion * GAME_CONSTANT.BLOCK_SIZE, y: ypostion * GAME_CONSTANT.BLOCK_SIZE, nextDir: this.map[xpostion][ypostion]};
 }
