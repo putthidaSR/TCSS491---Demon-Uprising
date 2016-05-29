7ec04b309dfa50b83452ab341e9430c43bfa1aea
zhouzhaoxu <zhouzhaoxu@yahoo.com>
2016-05-28 17:41:56 -0700

    Update gameover.js

--- gameover.js
+++ gameover.js
@@ -12,12 +12,12 @@ GameOver.prototype.update = function () {
 	
     Entity.prototype.update.call(this);
     if(BOARD_CONSTANT.HEALTH <= 0) {
-    	game_over = true;
+    	BOARD_CONSTANT.GAME_OVER = true;
     }
 };
 
 GameOver.prototype.draw = function (ctx) {
-	if(game_over) {
+	if(BOARD_CONSTANT.GAME_OVER) {
 		this.gameOver = new Animation(this.spritesheet, 333, 289, 620, 120, 1, 0.30, 1, true);
 		this.gameOver.drawFrame(this.game.clockTick, ctx, 200, 300, 0.6);
 		Entity.prototype.draw.call(this)
