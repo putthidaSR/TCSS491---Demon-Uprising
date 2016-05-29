4a9546a8e83921d9a51846777a37ba650d617b00
zhouzhaoxu <zhouzhaoxu@yahoo.com>
2016-05-28 17:41:36 -0700

    Update main.js
    new map

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var AM = new AssetManager();
 var gameEngine = new GameEngine();
-var game_over = false;
 
+//The varibable for all the map
 var MAP = {
 	FIRSTMAP: [
 "     sssss                        ",
@@ -16,8 +16,8 @@ var MAP = {
 "                    ddddd         ",
 "                    ddddd         ",
 "                    ddddd         ",
-"   t                ddddd         ",
-"   v                ddddd         ",
+"                    ddddd         ",
+"                    ddddd         ",
 "                    ddddd         ",
 "          dlllllllllldddd         ",
 "          dddlllllllllldd         ",
@@ -25,10 +25,136 @@ var MAP = {
 "          ddddd                   ",
 "          ddddd                   ",
 "          ddddd                   ",
-"          ddddd                   ",
 "          eeeee                   ",
+"          eemee                   ",
 "          eeeee                   ",
     ],
+    SECONDMAP: [
+"     sssss                        ",
+"     ddddd                        ",
+"     ddddd                        ",
+"     ddddd                        ",
+"     ddddd                        ",
+"     ddddrrrrrrrrrrrrrrrd         ",
+"     dddrrrrrrrrrrrrrrddd         ",
+"     ddrrrrrrrrrrrrrddddd         ",
+"     ddddd          ddddd         ",
+"     drddd          ddddd         ",
+"     ddddd          ddddd         ",
+"     ddddd          ddddd         ",
+"     ddddd          ddddd         ",
+"     ddddd          ddddd         ",
+"     ddddd          ddddd         ",
+"     ddrrrrrrrrrrrrrddddd         ",
+"     ddrrrrrrrrrrrrrddddd         ",
+"     rrrrrrrrrrrrrrrddddd         ",
+"                    ddddd         ",
+"                    ddddd         ",
+"                    ddddd         ",
+"                    eeeee         ",
+"                    eemee         ",
+"                    eeeee         ",
+    ],
+    THIRDMAP: [
+"     sss                 sss      ",
+"     ddd                 ddd      ",
+"     ddd                 ddd      ",
+"     ddd                 ddd      ",
+"     ddd                 ddd      ",
+"     ddd                 ddd      ",
+"     ddd                 ddd      ",
+"     ddd                 ddd      ",
+"     ddd                 ddd      ",
+"     ddd                 ddd      ",
+"     ddrrrrrrrrdlllllllllldd      ",
+"     drrrrrrrrdddlllllllllld      ",
+"     rrrrrrrrdddddllllllllll      ",
+"             ddddd                ",
+"             ddddd                ",
+"             ddddd                ",
+"             ddddd                ",
+"             ddddd                ",
+"             ddddd                ",
+"             ddddd                ",
+"             ddddd                ",
+"             eeeee                ",
+"             eemee                ",
+"             eeeee                ",
+    ],
+    FOURTHMAP: [
+"                         sss      ",
+"                         ddd      ",
+"                         ddd      ",
+"srrrrrrrrrrrdllllllllllllddd      ",
+"srrrrrrrrrrdddlllllllllllldd      ",
+"srrrrrrrrrdddddllllllllllddd      ",
+"          ddddd          ddd      ",
+"          ddddd          ddd      ",
+"          ddddd          ddd      ",
+"          ddddd          ddd      ",
+"          ddddd          ddd      ",
+"          dddddlllllllllllldllllls",
+"          dddddlllllllllllllllllls",
+"          dddddlllllllllllllllllls",
+"          ddddd                   ",
+"          ddddd                   ",
+"          ddddd                   ",
+"          ddddd                   ",
+"          ddddd                   ",
+"          ddddd                   ",
+"          ddddd                   ",
+"          eeeee                   ",
+"          eemee                   ",
+"          eeeee                   ",
+    ],
+    FIFTHMAP: [
+"         ss              ss       ",
+"         dd              dd       ",
+"         dd              dd       ",
+"         drrrrrd         dd       ",
+"         rrrrrdd         dd       ",
+"srrrrrd       dd         dd       ",
+"srrrrdd       dd         dd       ",
+"     dd       dd         dd       ",
+"     dd       dddllllllllld  dllls",
+"     dd       ddddlllllllll  ddlls",
+"     dd       ddddd          dd   ",
+"     dd       ddddd          dd   ",
+"     drrrrrrrrrddddllllllllllld   ",
+"     rrrrrrrrrdddddllllllllllll   ",
+"              ddddd               ",
+"              ddddd               ",
+"              ddddd               ",
+"              ddddd               ",
+"              ddddd               ",
+"              ddddd               ",
+"              ddddd               ",
+"              eeeee               ",
+"              eemee               ",
+"              eeeee               ",
+    ],
+};
+
+//Constant for the board
+var BOARD_CONSTANT = {
+    //current money
+    MONEY: 100,
+    //cost of the tower
+    TOWER_COST: [0, 25, 50 ,75],
+    //range of the tower
+    TOWER_RANGE: [0, 100, 150 ,200],
+    //tower type selector.
+    CURRENTTOWER: 0,
+    //User's health
+    HEALTH: 10,
+    //current level
+    LEVEL: 1,
+    //gameover?
+    GAME_OVER: false,
+    //current map
+    CURRENT_MAP: MAP.FIRSTMAP,
+    //when magician died
+    FINISH: false,
 };
 
 /*
@@ -61,24 +187,15 @@ AM.queueDownload("./img/coin.png");
 AM.queueDownload("./img/heart.png");
 AM.queueDownload("./img/tileSheet.jpg");
 AM.queueDownload("./img/Game_Over.png");
-//AM.queueDownload("./img/gameboard.jpg");
 
 //agents
 AM.queueDownload("./img/magician.png");
 AM.queueDownload("./img/magician2.png");
 
-AM.queueDownload("./img/human1left.png");
-AM.queueDownload("./img/human1right.png");
-
 AM.queueDownload("./img/human6walkback.png");
 AM.queueDownload("./img/human6walkfront.png");
 AM.queueDownload("./img/human6walkleft.png");
 AM.queueDownload("./img/human6walkright.png");
-
-AM.queueDownload("./img/human9back.png");
-AM.queueDownload("./img/human9front.png");
-AM.queueDownload("./img/human9left.png");
-AM.queueDownload("./img/human9right.png");
 
 //tower
 AM.queueDownload("./img/tower1.png");
@@ -101,107 +218,14 @@ AM.queueDownload("./img/tree2.png");
 	gameEngine.init(ctx);
 	gameEngine.start();
     
-    gameEngine.addEntity(new Background(gameEngine, AM.getAsset("./img/tileSheet.jpg"), MAP.FIRSTMAP));
-	gameEngine.addEntity(new GameBoard(gameEngine, MAP.FIRSTMAP));   
+    gameEngine.addEntity(new Background(gameEngine));
+	gameEngine.addEntity(new GameBoard(gameEngine));   
     gameEngine.addEntity(new Spell(gameEngine, AM.getAsset("./img/fireball_0.png")));
     gameEngine.addEntity(new GameOver(gameEngine, AM.getAsset("./img/Game_Over.png")));
-    
-
-	//ani(gameEngine);
-
-	//Generate the monster every 500 miliseconds
-	//var generate = window.setInterval(spawn, 500);    
-
-	/*var monsterNum = 0;
-	function spawn() {
-		gameEngine.addEntity(new Human1(gameEngine, AM.getAsset("./img/walk1.png")));
-		monsterNum += 1;
-
-	}*/
 
 	console.log("All Done!");
 });
 
-var ani = function(gameEngine) {
-	
-	gameEngine.addEntity(new Background(gameEngine, AM.getAsset("./img/bgtest2.jpg")));
-
-	//gameEngine.addEntity(new Background(gameEngine, AM.getAsset("./img/gameboard.jpg")));
-	gameEngine.addEntity(new Magician(gameEngine, AM.getAsset("./img/magician.png")));
-	gameEngine.addEntity(new Human1(gameEngine, AM.getAsset("./img/walk1.png")));
-	gameEngine.addEntity(new Human1(gameEngine, AM.getAsset("./img/health_bar.png")));
-	gameEngine.addEntity(new Bird(gameEngine, AM.getAsset("./img/Animal.png")));
-	//gameEngine.addEntity(new Hero(gameEngine, AM.getAsset("./img/hero2.png")));
-	gameEngine.addEntity(new Demon(gameEngine, AM.getAsset("./img/demon.png")));
-	//gameEngine.addEntity(new Knight(gameEngine, AM.getAsset("./img/knightFT.png")));
-	gameEngine.addEntity(new Demon1(gameEngine, AM.getAsset("./img/demon1.png")));
-	gameEngine.addEntity(new Magician1(gameEngine, AM.getAsset("./img/Magician1.png")));
-	gameEngine.addEntity(new FireBall(gameEngine, AM.getAsset("./img/explosion.png")));
-	gameEngine.addEntity(new Light(gameEngine, AM.getAsset("./img/light.png")));
-	gameEngine.addEntity(new Snowball(gameEngine, AM.getAsset("./img/snowball.png")));
-	gameEngine.addEntity(new Spell(gameEngine, AM.getAsset("./img/fireball_0.png")));
-	gameEngine.addEntity(new Coin(gameEngine, AM.getAsset("./img/coin.png")));
-	gameEngine.addEntity(new Heart(gameEngine, AM.getAsset("./img/heart.png")));
-	
-	//Tower Object
-	gameEngine.addEntity(new TowerObject1(gameEngine, AM.getAsset("./img/tower1.png")));
-	//gameEngine.addEntity(new TowerObject2(gameEngine, AM.getAsset("./img/tower2.png")));
-	//gameEngine.addEntity(new TowerObject3(gameEngine, AM.getAsset("./img/tower3.png")));
-
-}
-
 var CANVAS_WIDTH = 1000;
 var CANVAS_HEIGHT = 700;
 
-var startGame = function (gameEngine) {
-
-	var background = new Background(gameEngine, AM.getAsset("./img/titleScreen1.png"), 1000, 600);
-
-
-}
-
-//Coin
-function Coin(game, spritesheet) {
-	this.animation = new Animation(spritesheet, 100, 100, 100, 0.08, 10, true, 0.4);
-	this.speed = 80;
-	this.ctx = game.ctx;
-	Entity.call(this, game, 960, 20);
-
-}
-
-Coin.prototype = new Entity();
-Coin.prototype.constructor = Coin;
-
-Coin.prototype.update = function () {
-	Entity.prototype.update.call(this);
-}
-
-Coin.prototype.draw = function () {
-	this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
-	Entity.prototype.draw.call(this);
-
-}
-
-//heart
-function Heart(game, spritesheet) {
-	this.animation = new Animation(spritesheet, 300, 250, 300, 0.08, 1, true, 0.12);
-	this.speed = 80;
-	this.ctx = game.ctx;
-	Entity.call(this, game, 910, 23);
-}
-
-Heart.prototype = new Entity();
-Heart.prototype.constructor = Heart;
-
-Heart.prototype.update = function () {
-	Entity.prototype.update.call(this);
-}
-
-
-Heart.prototype.draw = function () {
-	//this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
-	//Entity.prototype.draw.call(this);
-	//ctx.drawImage(AM.getAsset("./img/heart.png"), 900, 100);
-	this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
-	Entity.prototype.draw.call(this);
-}
