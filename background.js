1f4adbb706fcc73cb667bd2094ff2314b6586a4f
zhouzhaoxu <zhouzhaoxu@yahoo.com>
2016-05-28 17:39:38 -0700

    Update background.js
    new background

--- background.js
+++ background.js
@@ -1,8 +1,7 @@
-function Background(game, spritesheet, map) {
+function Background(game) {
     this.x = 0;
     this.y = 0;
-    this.map = map;
-    this.spritesheet = spritesheet;
+    this.map = MAP.FIRSTMAP;
     this.game = game;
     this.ctx = game.ctx;
     this.grass = new Animation(AM.getAsset("./img/tileSheet.jpg"), GAME_CONSTANT.BLOCK_GRASS_X, 
@@ -14,56 +13,47 @@ function Background(game, spritesheet, map) {
     		GAME_CONSTANT.BLOCK_SHEETWIDTH, GAME_CONSTANT.BLOCK_FRAMEDURATION, GAME_CONSTANT.BLOCK_FRAMES, 
     		GAME_CONSTANT.BLOCK_LOOP)
     
-    this.tree = new Animation(AM.getAsset("./img/tree1.png"),0, 
-    		0, 30, GAME_CONSTANT.BLOCK_SIZE, 
+    this.tree = new Animation(AM.getAsset("./img/tree1.png"),GAME_CONSTANT.BLOCK_TREE_X, 
+    		GAME_CONSTANT.BLOCK_TREE_Y, GAME_CONSTANT.BLOCK_SIZE, GAME_CONSTANT.BLOCK_SIZE, 
     		GAME_CONSTANT.BLOCK_SHEETWIDTH, GAME_CONSTANT.BLOCK_FRAMEDURATION, GAME_CONSTANT.BLOCK_FRAMES, 
     		GAME_CONSTANT.BLOCK_LOOP)
-    
-    this.tree1 = new Animation(AM.getAsset("./img/tree1.png"),0, 
-    		30, 30, GAME_CONSTANT.BLOCK_SIZE, 
-    		GAME_CONSTANT.BLOCK_SHEETWIDTH, GAME_CONSTANT.BLOCK_FRAMEDURATION, GAME_CONSTANT.BLOCK_FRAMES, 
-    		GAME_CONSTANT.BLOCK_LOOP)
-    //Entity.call(this, game, 350, 350);
 };
 
 Background.prototype = new Entity();
 Background.prototype.constructor = Background;
 
+//draw the background
 Background.prototype.draw = function (ctx) {
 	//set the map background
 	for(var row = 0; row < this.map.length; row++) {
 	    for(var col = 0; col < this.map[row].length; col++) {
-	    	
-
 	    	//empty space
-	        if(this.map[row][col] == " " || this.map[row][col] == "t" || this.map[row][col] == "v") {
+	        if(this.map[row][col] == " " || this.map[row][col] == "t") {
 	        	this.grass.drawFrame(this.game.clockTick, ctx, 
 	        			this.x + col * GAME_CONSTANT.BLOCK_SIZE, 
 	        			this.y + row * GAME_CONSTANT.BLOCK_SIZE);
         	//road
 	        } else if(this.map[row][col] == "r" || this.map[row][col] == "s" || this.map[row][col] == "l" 
-	        	|| this.map[row][col] == "u" || this.map[row][col] == "d" || this.map[row][col] == "e") {
+	        	|| this.map[row][col] == "u" || this.map[row][col] == "d" || this.map[row][col] == "e"
+                || this.map[row][col] == "m") {
 	        	this.road.drawFrame(this.game.clockTick, ctx, 
 	        			this.x + col * GAME_CONSTANT.BLOCK_SIZE, 
 	        			this.y + row * GAME_CONSTANT.BLOCK_SIZE);
-	        } 
-	    	if (this.map[row][col] == "t") {
-	        	this.tree.drawFrame(this.game.clockTick, ctx, 
-	        			this.x + col * GAME_CONSTANT.BLOCK_SIZE, 
-	        			this.y + row * GAME_CONSTANT.BLOCK_SIZE);
 	        }
-	    	if (this.map[row][col] == "v") {
-	        	this.tree1.drawFrame(this.game.clockTick, ctx, 
-	        			this.x + col * GAME_CONSTANT.BLOCK_SIZE, 
-	        			this.y + row * GAME_CONSTANT.BLOCK_SIZE);
-	        }
-	    	
-	        
 	    }
 	}
 	Entity.prototype.draw.call(this);
 };
 
+//update the background based on the level
 Background.prototype.update = function () {
+    switch(BOARD_CONSTANT.LEVEL) {
+        case 1: this.map = MAP.FIRSTMAP; break;
+        case 2: this.map = MAP.SECONDMAP; break;
+        case 3: this.map = MAP.THIRDMAP; break;
+        case 4: this.map = MAP.FOURTHMAP; break;
+        case 5: this.map = MAP.FIFTHMAP; break;
+        default: console.log("no map");
+    }
 	 Entity.prototype.update.call(this);
 };
