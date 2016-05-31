98d1f46407f95b87fb4dbccdcbff247b29535780
zhouzhaoxu <zhouzhaoxu@yahoo.com>
2016-05-30 18:45:31 -0700

    Update bullets.js
    added comment

--- agents/TowerShoot/bullets.js
+++ agents/TowerShoot/bullets.js
@@ -1,12 +1,16 @@
 function Bullet(game, board, x, y, target, damage, id) {
 	this.x = x;
 	this.y = y;
+	//the target the bullet need to hit
 	this.target = target;
+	//the damage of the bullet
 	this.damage = damage;
 	this.game = game;
-	this.speed = 5*20/((id+1)*10);
+	//the speed of the bullet
+	this.speed = 10 /((5 - id));
+	//the size of the bullet
 	this.radius = 7 *(id+1);
-	this.done = false;;
+	this.done = false;
 };
 
 Bullet.prototype = new Entity();
@@ -17,13 +21,12 @@ Bullet.prototype.update = function () {
 	var check = true;;
 	for (var i = 0; i < this.game.entities.length; i++) {
         var ent = this.game.entities[i];
+        //check to see if the bullet had finish its job of hitting their target
         if(ent == this && this.done) {
-        	this.game.entities[i].removeFromWorld = true;
-        	
+        	this.game.entities[i].removeFromWorld = true;       	
             break;
         }
         if(ent == this.target) {
-        	
         	check = false;
             //check to see if it is within range 
             this.move();
@@ -33,23 +36,14 @@ Bullet.prototype.update = function () {
             		ent.isAlive = false;
             		this.game.entities[i].removeFromWorld = true;
             		BOARD_CONSTANT.MONEY += 5;
-            		//console.log("Money = " + BOARD_CONSTANT.MONEY);
-        			document.getElementById('money').innerHTML = BOARD_CONSTANT.MONEY;
-            	
-                //	this.game.entities[i].HEALTH_WIDTH -= 15;
-
-                	
+        			document.getElementById('money').innerHTML = BOARD_CONSTANT.MONEY;	
             	}
-            	this.done = true;
-            	
-            	
+            	this.done = true;	
             }
         }
     }
 	if(check) {
-		
-		this.done = true;
-		
+		this.done = true;	
 	}
 }
 
@@ -63,17 +57,17 @@ Bullet.prototype.move = function() {
 	this.y = this.y+this.speed*yDist/dist;
 };
 
-Bullet.prototype.draw = function() {
+Bullet.prototype.draw = function(ctx) {
+	//draw the bullet
 	if(!this.done) {
-		context.beginPath();
-		context.arc(this.x,this.y,this.radius,0,2*Math.PI);
-		context.fillStyle='red';
-		context.fill();
+		ctx.beginPath();
+		ctx.arc(this.x,this.y,this.radius,0,2*Math.PI);
+		ctx.fillStyle='red';
+		ctx.fill();
 	}
 };
 
 Bullet.prototype.checkCollision = function() {
-
 	var xDist = this.target.x + this.target.size - this.x; 
 	var yDist = this.target.y + this.target.size - this.y;
 	var dist = Math.sqrt(xDist*xDist+yDist*yDist);
@@ -81,15 +75,5 @@ Bullet.prototype.checkCollision = function() {
 	if(dist < 20) {
 		return true;
 	}
-	
-	// if(this.x <= this.target.x + this.target.size &&
-// 			this.x + this.radius >= this.target.x &&
-// 			this.y <= this.target.y + this.target.size &&
-// 			this.y + this.radius >= this.target.y) {
-// 		return true;
-// 		
-// 	}
-	
 	return false;
-	
 };
